37faa596a5b453896e5eaf2b1675e806558c7982
Geoffrey Ouvrard <geoffrey.1796@gmail.com>
2023-03-09 14:03:36 +0100

    code cleanup on layout of AllDayEvents widget

---
+++ lib/widget/week_view/all_day_events.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_calendar/util/layout_util.dart';
+import 'package:flutter_calendar/util/week_day.dart';
+
+import '../../model/event.dart';
+import '../../util/date_util.dart';
+
+class AllDayEvents extends StatelessWidget {
+  const AllDayEvents({super.key, required this.events});
+  final List<Event> events;
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (ctx, constrainst) {
+      final dayWidth = constrainst.maxWidth / 7.0;
+      return Stack(
+        children: List.generate(events.length, (i) {
+          final e = events[i];
+          var top = 0.0;
+          var bottom = 0.0;
+          final left = getWeekDayFromDate(e.start).helper.index * dayWidth +
+              (e.start.hour / 24.0) * dayWidth +
+              (e.start.minute / (60.0 * 24.0)) * dayWidth;
+          final duration = e.end.difference(e.start);
+          final width = (duration.inMinutes / (60.0 * 24.0)) * dayWidth;
+          if (events.length % 2 == 0) {
+            top = (cellHeight / 2) * i;
+            bottom = (cellHeight / 2) * (events.length - (i + 1));
+          } else {
+            top = (cellHeight / (events.length - 1)) * i;
+            bottom =
+                (cellHeight / (events.length - 1)) * (events.length - (i + 1));
+          }
+          return Positioned(
+            top: top,
+            bottom: bottom,
+            left: left,
+            width: width,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(Radius.circular(4.0)),
+                color: e.color,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Text(e.title),
+              ),
+            ),
+          );
+        }).toList(),
+      );
+    });
+  }
+}
